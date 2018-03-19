6b0affba55f3cbdaf4ad93824392abd6fda3b1f2
Brett Cimbalik <bcimbali@gmail.com>
2018-03-18 20:56:58 -0500

    unanswered questions now tabulate in results

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     // Global Variables //////////////////////////////////////////////////////////////////////
 
-    // Question data:
+    // Question data as objects in an array:
     let questions = [ 
         {
             "question": "According to various international studies, the worlds most popular color is...",
@@ -31,11 +31,19 @@ $(document).ready(function() {
 
     // Displays the totals at the end of the game
 
+    // Function looping thru the length of the questions, add a new question into a new div, loop thru all of
+    // the possible answers in the answer array, add a new radio button
     function populateQuestions() {
-        
+        for (var i = 0; i < questions.length; i++) {
+            $('.js-questions').append('<p>' + questions[i].question + '</p>');
+                for (var j = 0; j < questions[i].answer.length; j++) {
+                    $('.js-questions').append('<input type="radio" value="' + questions[i].answer[j] + '"name="'+ i + '">' + questions[i].answer[j] + '</input>' );
+                }
+            $('.js-questions').append('<br><hr>');
+        }
     }
 
-    function showTotals() {
+    function displayTotals() {
         $('<div>All Done!</div>').appendTo('.js-main-section');
         $('<div>Correct Answers: ' + answersGuessedCorrect + '</div>').appendTo('.js-main-section');
         $('<div>Incorrect Answers: ' + answersGuessedIncorrect + '</div>').appendTo('.js-main-section');
@@ -52,21 +60,9 @@ $(document).ready(function() {
 
     // Starts the game
     function startGame() {
-        // Hide the start button
         hideStartButton();
-        // Make the stop button visible
         showStopButton();
-        // Looping thru the length of the questions, add a new question into a new div, loop thru all of
-        // the possible answers in the answer array, add a new radio button
-        for (var i = 0; i < questions.length; i++) {
-            $('.js-questions').append('<p>' + questions[i].question + '</p>');
-                for (var j = 0; j < questions[i].answer.length; j++) {
-                    $('.js-questions').append('<input type="radio" value="' + questions[i].answer[j] + '"name="'+ i + '">' + questions[i].answer[j] + '</input>' );
-                }
-            $('.js-questions').append('<br><hr>');
-            // loop through answers
-            // $('.js-answers').append('<input type="radio">'+ questions[i].answer[0] +'</input>');
-        }
+        populateQuestions();
     }
 
     // Stops the game
@@ -74,32 +70,24 @@ $(document).ready(function() {
 
         $('input:checked').each(function() {
             // if value = correct quesiton answer add 1 to score
+            // console.log($(this));
             let answerChecked = $(this).val();
                 if (answerChecked === questions[$(this).attr('name')].correctAnswer) {
-                    console.log('correct');
+                    // console.log('correct');
                     answersGuessedCorrect++;
-                    console.log('Answers Corect: ' + answersGuessedCorrect);
+                    // console.log('Answers Corect: ' + answersGuessedCorrect);
                 }
                 else {
-                    console.log('wrong');
+                    // console.log('wrong');
                     answersGuessedIncorrect++;
-                    console.log('Answers Incorrect: ' + answersGuessedIncorrect);
-                }
-        $('.js-main-section').children().hide();
+                    // console.log('Answers Incorrect: ' + answersGuessedIncorrect);
+                }                
+            $('.js-main-section').children().hide();
             // console.log($(this).val());
-        });
+        }); 
 
-        // $.each($('input[name="a0"]:checked')), function() {
-        //     $(this);
-        // }
-        // for (var i; i < questions.length; i++) {
-        //     for (var b; b < questions[i].answer.length; b++) {
-        //        if ($('input[name=a' + i + ']:checked')) {
-
-        //             console.log('')
-        //         }
-        //     }
-        // }
+        let totalAnswersAttempted = answersGuessedCorrect + answersGuessedIncorrect;
+        answersUnanswered = questions.length - totalAnswersAttempted;
     }
 
 
@@ -110,32 +98,31 @@ $(document).ready(function() {
         // On page load, show click to start button.
 
         $('.js-start').on('click', function() {
-            // execute instructions
             startGame();
         });
 
-        // On button click, Pages loads with trivia questions and timer starts counting down with 120 seconds.
 
         $('.js-stop').on('click', function() {
-            // execute instructions
             stopGame();
             $(this).css('visibility', 'hidden');
-            showTotals();
+            displayTotals();
         });
 
-        // Between 5-10 questions load on the page and each question has 4 radio buttons.
-
-        // User clicks radio button for each answer to a question
-
-        // User clicks done button, and it submits the answers and evaluates questions right, questions wrong, and unanswered. Displays them on a page.
-
-
         // If timer runs out, the form is submited and evaluates questions right, wrong and unanswered. Displays them on a page.
-
-
 
 });
 
 
 
 
+// $.each($('input[name="a0"]:checked')), function() {
+        //     $(this);
+        // }
+        // for (var i; i < questions.length; i++) {
+        //     for (var b; b < questions[i].answer.length; b++) {
+        //        if ($('input[name=a' + i + ']:checked')) {
+
+        //             console.log('')
+        //         }
+        //     }
+        // }
